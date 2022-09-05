c62c6d7315cb1f8af977341ecd66cdf4ec15db7e
alexander <einfachalex@sags-per-mail.de>
2022-09-05 23:56:13 +0200

    add controller to start an encoding and put it in queue

---
+++ BlazorFFMPEG.Backend/Controllers/Post/StartEncodeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using BlazorFFMPEG.Backend.Database;
+using BlazorFFMPEG.Backend.Modules.Jobs;
+using BlazorFFMPEG.Backend.Modules.ServerLoad;
+using EinfachAlex.Utils.Logging;
+using EinfachAlex.Utils.WebRequest;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorFFMPEG.Backend.Controllers.Post
+{
+    public class StartEncodeController : ControllerBase
+    {
+        private const string ENDPOINT = "/startEncode";
+        private const ERequestTypes ENDPOINT_TYPE = ERequestTypes.POST;
+
+        [HttpPost(ENDPOINT)]
+        public ContentResult PostStartEncode([FromForm] string codec, [FromForm] string inputFile)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            LoggerCommonMessages.logEndpointRequest(ENDPOINT, ENDPOINT_TYPE);
+
+            if (codec == null || inputFile == null) return Content("No parameters given");
+            
+            using (databaseContext databaseContext = new databaseContext())
+            {
+                EncodeJobManager.getInstance()!.addEncodeJob(databaseContext, codec, inputFile);
+                databaseContext.SaveChanges();
+            }
+
+            sw.Stop();
+            Logger.v($"{sw.ElapsedMilliseconds} ms");
+            ServerLoadAnalyzer.getInstance().handleLoad(sw.ElapsedMilliseconds);
+
+            return Content("JSON");
+        }
+    }
+}
---
+++ BlazorFFMPEG.Backend/Modules/Jobs/EncodeJobManager.cs
@@ -0,0 +1,20 @@
+﻿using BlazorFFMPEG.Backend.Database;
+
+namespace BlazorFFMPEG.Backend.Modules.Jobs;
+
+public class EncodeJobManager
+{
+    private static EncodeJobManager? instance;
+
+    public static EncodeJobManager? getInstance()
+    {
+        return instance ??= new EncodeJobManager();
+    }
+
+    public Task addEncodeJob(databaseContext databaseContext, string codec, string file)
+    {
+        EncodeJob.constructNew(databaseContext, false, codec, file);
+
+        return Task.CompletedTask;
+    }
+}
