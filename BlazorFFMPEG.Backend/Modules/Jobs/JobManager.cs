3e0dbd18e886434430268641b85c96b77ad911eb
alexander <einfachalex@sags-per-mail.de>
2022-09-06 00:33:45 +0200

    start jobs in job manager

---
+++ BlazorFFMPEG.Backend/Modules/Jobs/JobManager.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorFFMPEG.Backend.Modules.Jobs;
+
+public class JobManager
+{
+    private static JobManager instance;
+
+    public static JobManager getInstance()
+    {
+        return instance ??= new JobManager();
+    }
+
+    public void startJobThreads()
+    {
+        startQueueScanner();
+    }
+
+    private void startQueueScanner()
+    {
+        Thread thread = new Thread(QueueScannerJob.getInstance().startScanning);
+        thread.Start();
+    }
+}
