9ed9cf97df084ed054674e8ddaa3ead2ac8d9944
alexander <einfachalex@sags-per-mail.de>
2022-09-05 23:48:30 +0200

    construct method for encode jobs

---
+++ BlazorFFMPEG.Backend/Database/_Encodejob.cs
@@ -0,0 +1,41 @@
+﻿using BlazorFFMPEG.Backend.Modules.Jobs;
+using EinfachAlex.Utils.HashGenerator;
+using EinfachAlex.Utils.Logging;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorFFMPEG.Backend.Database;
+
+public partial class EncodeJob
+{
+    public static EncodeJob constructNew(databaseContext databaseContext, bool commit, string codec, string file)
+    {
+        LoggerCommonMessages.logConstructNew(file);
+
+        Hash id = generateId(codec, file);
+
+        EncodeJob proxyObject = new EncodeJob()
+        {
+            Codec = codec,
+            Path = file
+        };
+
+        var proxy = databaseContext.CreateProxy<EncodeJob>();
+        databaseContext.Entry(proxy).CurrentValues.SetValues(proxyObject);
+
+        databaseContext.Add(proxy);
+
+        if(commit) databaseContext.SaveChanges();
+
+        return proxy;
+    }
+    
+    private static Hash generateId(string codec, string file)
+    {
+        string key = $"{codec}{file}";
+
+        Hash id = HashGenerator.generateSHA256(key);
+        LoggerCommonMessages.logGeneratedId(id);
+
+        return id;
+    }
+}
