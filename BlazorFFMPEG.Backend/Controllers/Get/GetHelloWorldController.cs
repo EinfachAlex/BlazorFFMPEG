1c43c0044e9ca20349b6fc76a58af1f5c89c4865
alexander <einfachalex@sags-per-mail.de>
2022-09-05 20:26:46 +0200

    add helloworld controller

---
+++ BlazorFFMPEG.Backend/Controllers/Get/GetHelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using EinfachAlex.Utils.Logging;
+using EinfachAlex.Utils.WebRequest;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BlazorFFMPEG.Backend.Controllers.Get
+{
+    public class GetHelloWorldController : ControllerBase
+    {
+        private const string ENDPOINT = "/helloWorld";
+        private const ERequestTypes ENDPOINT_TYPE = ERequestTypes.GET;
+
+        [HttpGet(ENDPOINT)]
+        public string GetHelloWorld()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            LoggerCommonMessages.logEndpointRequest(ENDPOINT, ENDPOINT_TYPE);
+
+            string response = "Hello World!";
+            
+            sw.Stop();
+            Logger.v($"{sw.ElapsedMilliseconds} ms");
+
+            return response;
+        }
+    }
+}
