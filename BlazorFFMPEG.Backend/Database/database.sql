a9192c6ab7ee05b96d669a702adca915ce40cca7
alexander <einfachalex@sags-per-mail.de>
2022-09-05 23:47:30 +0200

    add first database file

--- BlazorFFMPEG.Backend/Database/database.sql
+++ BlazorFFMPEG.Backend/Database/database.sql
@@ -1,19 +1,26 @@
 ﻿DROP TABLE IF EXISTS encode_jobs;
 DROP TABLE IF EXISTS constants_status;
 
-CREATE TABLE constants_status(
-                                 id SERIAL PRIMARY KEY,
-                                 description TEXT
+CREATE TABLE constants_status
+(
+    id          SERIAL PRIMARY KEY,
+    description TEXT
 );
 
-CREATE TABLE encode_jobs(
-                            jobId SERIAL PRIMARY KEY,
-                            status SERIAL,
+CREATE TABLE encode_jobs
+(
+    jobId  SERIAL PRIMARY KEY,
+    status SERIAL,
+    codec TEXT,
+    path TEXT,
 
-                            CONSTRAINT fk_status FOREIGN KEY(status) REFERENCES constants_status(id)
+    CONSTRAINT fk_status FOREIGN KEY (status) REFERENCES constants_status (id)
 );
 
-INSERT INTO constants_status(description) VALUES ('New');
-INSERT INTO constants_status(description) VALUES ('Working');
-INSERT INTO constants_status(description) VALUES ('Finished');
+INSERT INTO constants_status(description)
+VALUES ('New');
+INSERT INTO constants_status(description)
+VALUES ('Working');
+INSERT INTO constants_status(description)
+VALUES ('Finished');
 
