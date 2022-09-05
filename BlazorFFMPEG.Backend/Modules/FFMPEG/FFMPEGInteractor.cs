cf88ab5e5bf7380e26b33dc1a9d77b0f3714e064
alexander <einfachalex@sags-per-mail.de>
2022-09-05 23:50:59 +0200

    method to call ffmpeg to start an encode

---
+++ BlazorFFMPEG.Backend/Modules/FFMPEG/FFMPEGInteractor.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace BlazorFFMPEG.Backend.Modules.FFMPEG;
+
+public class FFMPEG
+{
+    public async Task startEncode(string inputFile, string codec)
+    {
+        Process ffmpegProcess = new Process
+        {
+            StartInfo =
+            {
+                CreateNoWindow = true,
+                ErrorDialog = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            }
+        };
+
+        ffmpegProcess.OutputDataReceived += (_, args) => Console.WriteLine(args.Data);
+        ffmpegProcess.ErrorDataReceived += (_, args) => Console.WriteLine(args.Data);
+
+        ffmpegProcess.StartInfo.FileName = "ffmpeg";
+        ffmpegProcess.StartInfo.Arguments = $" -i {inputFile} -c:v {codec} out.mp4";
+
+        ffmpegProcess.Start();
+
+        ffmpegProcess.BeginErrorReadLine();
+        ffmpegProcess.BeginOutputReadLine();
+    }
+}
