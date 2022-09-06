60e4ddb4d0cd256611955550dda0825a154510ce
alexander <einfachalex@sags-per-mail.de>
2022-09-07 00:00:35 +0200

    start job threads

--- BlazorFFMPEG.Backend/Program.cs
+++ BlazorFFMPEG.Backend/Program.cs
@@ -1,3 +1,5 @@
+using BlazorFFMPEG.Backend.Modules.Jobs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +23,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+JobManager.getInstance().startJobThreads();
 
 app.Run();
