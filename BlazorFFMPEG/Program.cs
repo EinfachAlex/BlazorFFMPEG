d7d9b54b8e60211a9ffaaa7e9b7546ae70f1bc5d
alexander <einfachalex@sags-per-mail.de>
2022-09-06 20:21:04 +0200

    move codec selection into own component

--- BlazorFFMPEG/Program.cs
+++ BlazorFFMPEG/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<FfmpegCodecService>();
 
 var app = builder.Build();
 
