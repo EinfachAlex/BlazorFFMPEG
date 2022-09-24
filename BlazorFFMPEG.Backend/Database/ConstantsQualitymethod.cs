5fc65f874159acb05cce9acdbaba6b02a1501e7e
alexander <einfachalex@sags-per-mail.de>
2022-09-24 23:19:05 +0200

    add constantsqualitymethod database object

--- BlazorFFMPEG.Backend/Database/ConstantsQualitymethod.cs
+++ BlazorFFMPEG.Backend/Database/ConstantsQualitymethod.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 
 namespace BlazorFFMPEG.Backend.Database
 {
-    public partial class QualityMethod
+    public partial class ConstantsQualitymethod
     {
-        public QualityMethod()
+        public ConstantsQualitymethod()
         {
             EncodeJobs = new HashSet<EncodeJob>();
         }
 
         public int Id { get; set; }
         public string? Description { get; set; }
+        public int? Minqualityvalue { get; set; }
+        public int? Maxqualityvalue { get; set; }
 
         public virtual ICollection<EncodeJob> EncodeJobs { get; set; }
     }
